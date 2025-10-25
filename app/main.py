12560cd8c6e503a99317bcc5f9316721b2687972
Mones26 <jageitos@andrew.cmu.edu>
2025-10-24 23:37:24 -0400

    Working main microservice

--- app/main.py
+++ app/main.py
@@ -12,6 +12,10 @@ def read_root():
 RECITATION_HOURS = {"a": "09:00~09:50", "b": "10:00~10:50",
                     "c": "11:00~11:50", "d": "1:00~1:50",
                     "e": "2:00~2:50", "f": "3:00~3:50"}
+RECITATION_TAs = {"a": ["Juan", "---"], "b": ["Kareem", "Jonathan"],
+                    "c": ["Juan", "Rathi"], "d": ["Sarah", "Savannah"],
+                    "e": ["Sarah", "Rathi"], "f": ["Bing", "Jonathan"]}
+
 MICROSERVICE_LINK = "https://whos-my-ta.fly.dev/section_id/"
 
 
@@ -23,24 +27,25 @@ def get_section_info(section_id: str):
 
     section_id = section_id.lower()
 
-    response = requests.get(MICROSERVICE_LINK + section_id)
+    # response = requests.get(MICROSERVICE_LINK + section_id)
 
-    # You can check out what the response body looks like in terminal using the print statement
-    data = response.json()
-    print(data)
-    ta_name_list = data["ta_names"]
-    ta1_name = ta_name_list[0]["fname"] + " " + ta_name_list[0]["lname"]
-    ta2_name = ta_name_list[1]["fname"] + " " + ta_name_list[1]["lname"]
+    # # You can check out what the response body looks like in terminal using the print statement
+    # data = response.json()
+    # print(data)
+    # ta_name_list = data["ta_names"]
+    # ta1_name = ta_name_list[0]["fname"] + " " + ta_name_list[0]["lname"]
+    # ta2_name = ta_name_list[1]["fname"] + " " + ta_name_list[1]["lname"]
 
-    print(ta1_name)
+    # print(ta1_name)
 
     # TODO
-    if section_id == "a":
+    
+    if section_id in RECITATION_HOURS:
         return {
-            "section": "section_name",
-            "start_time": "HH:MM",
-            "end_time": "HH:MM",
-            "ta": ["taName1", "taName2"]
+            "section": section_id,
+            "start_time": RECITATION_HOURS[section_id].split("~")[0],
+            "end_time": RECITATION_HOURS[section_id].split("~")[1],
+            "ta": RECITATION_TAs[section_id]
         }
     else:
         raise HTTPException(status_code=404, detail="Invalid section id")
